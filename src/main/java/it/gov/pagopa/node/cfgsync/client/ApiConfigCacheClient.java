9355167678dd5803feff5000300570f84ce23fe7
Francesco Parisi <francesco.parisi@tasgroup.eu>
2024-03-05 16:51:25 +0100

    NOD-690: fix cache uri

--- src/main/java/it/gov/pagopa/node/cfgsync/client/ApiConfigCacheClient.java
+++ src/main/java/it/gov/pagopa/node/cfgsync/client/ApiConfigCacheClient.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ApiConfigCacheClient {
 
-  @RequestLine("GET /cache")
+  @RequestLine("GET /")
   @Headers({
           "Ocp-Apim-Subscription-Key: {subscriptionKey}"
   })
