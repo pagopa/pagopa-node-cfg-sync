43a7aea9739a9ce83373c51f9d709636b33224c9
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-05-06 19:45:57 +0200

    added timezone

--- src/main/java/it/gov/pagopa/node/cfgsync/Application.java
+++ src/main/java/it/gov/pagopa/node/cfgsync/Application.java
@@ -1,13 +1,27 @@
 package it.gov.pagopa.node.cfgsync;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @Slf4j
 public class Application {
+
+    @Value("${timezone}")
+    private String timezone;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone(timezone));
     }
 }
